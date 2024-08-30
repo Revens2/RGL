2618b371b0b3ed1f770f6b950fa1e635042f4027
Juliann Ploquin <juliann.ploquin@gmail.com>
2024-08-30 12:19:51 +0200

    db

--- db_connect.php
+++ db_connect.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "root";
-$dbname = "gestionprojetsfacturation";
+$dbname = "rgl";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
